6b51c229505ac9445cde493b528124a8c07f9828
Takaki Ueno <ueknot@gmail.com>
2020-12-30 23:12:28 +0000

    Add english translation

--- dronedoc/source/conf.py
+++ dronedoc/source/conf.py
@@ -63,7 +63,9 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-#language = u'ja'
+language = u'ja'
+gettext_compact = False
+locale_dirs = ["../locales/"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
